e6a737853f089f61ad6b977f363018258ac294f3
SEANVSA <seanvs06@gmail.com>
2025-12-17 23:55:56 +0700

    destroy udh hapus data

--- LOGIN/logout.php
+++ LOGIN/logout.php
@@ -1,11 +1,6 @@
 <?php
-session_start(); // 1. Mulai session untuk mengakses data saat ini
-
-// 2. Hapus semua data session
-session_unset(); 
+session_start();
 session_destroy(); 
-
-// 3. Arahkan (Redirect) kembali ke file login.php
 header("Location: login.php"); 
 exit();
 ?>
